dbd5637544dadc966a6f59bb12313d04d0ea70f3
songning <songning@kedacom.com>
2019-06-27 10:31:17 +0800

    登陆页

---
+++ src/views/login/WelcomeTo.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class='welcome-to'></div>
+</template>
+
+<script>
+    import '@/style/login/welcomeTo.scss';
+
+    export default {
+        name: 'WelcomeTo'
+    };
+</script>
