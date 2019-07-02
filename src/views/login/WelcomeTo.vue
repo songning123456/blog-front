647206f2300e423def125e6e1d586c150576ad93
songning <songning@kedacom.com>
2019-07-02 19:46:27 +0800

    添加登录页图片 修改内存大小

--- src/views/login/WelcomeTo.vue
+++ src/views/login/WelcomeTo.vue
@@ -1,6 +1,23 @@
 <template>
     <div class='welcome-to'>
-        <img src="../../assets/login.jpg">
+        <div class='login-image'>
+            <img src="../../assets/login.jpg">
+        </div>
+        <div class='login-info'>
+            <el-col>
+                <el-row class='inner-image'>
+                    <el-row><img src='../../assets/头像.png'></el-row>
+                    <el-row>登陆</el-row>
+                </el-row>
+                <el-row>
+                    <el-input placeholder="请输入用户名" v-model="user.name" clearable></el-input>
+                    <img src='../../assets/头像inner.png'>
+                </el-row>
+                <el-row>
+                    <el-input placeholder="请输入密码" v-model="user.password" show-password></el-input>
+                </el-row>
+            </el-col>
+        </div>
     </div>
 </template>
 
@@ -8,7 +25,15 @@
 
 
     export default {
-        name: 'WelcomeTo'
+        name: 'WelcomeTo',
+        data () {
+            return {
+                user: {
+                    name: '',
+                    password: ''
+                }
+            };
+        }
     };
 </script>
 
