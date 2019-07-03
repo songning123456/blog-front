bfc747c60ff8062c0c841b51100ee085004afabb
songning <songning@kedacom.com>
2019-07-03 09:31:03 +0800

    登陆页面初稿

--- src/views/login/WelcomeTo.vue
+++ src/views/login/WelcomeTo.vue
@@ -4,25 +4,41 @@
             <img src="../../assets/login.jpg">
         </div>
         <div class='login-info'>
-            <el-col>
-                <el-row class='inner-image'>
-                    <el-row><img src='../../assets/头像.png'></el-row>
-                    <el-row>登陆</el-row>
-                </el-row>
-                <el-row>
-                    <el-input placeholder="请输入用户名" v-model="user.name" clearable></el-input>
-                    <img src='../../assets/头像inner.png'>
-                </el-row>
-                <el-row>
-                    <el-input placeholder="请输入密码" v-model="user.password" show-password></el-input>
-                </el-row>
-            </el-col>
+            <el-row class='outer-image'>
+                <el-row><img src='../../assets/头像.png'></el-row>
+                <el-row>登陆</el-row>
+            </el-row>
+            <el-row class='name-image'>
+                <el-input placeholder="请输入用户名" v-model="user.name" clearable>
+                    <i slot="prefix" class="el-input__icon">
+                        <img src='../../assets/头像inner.png'/></i>
+                </el-input>
+            </el-row>
+            <el-row class='password-image'>
+                <el-input placeholder="请输入密码" v-model="user.password" show-password>
+                    <i slot="prefix" class="el-input__icon">
+                        <img src='../../assets/密码.png'/></i>
+                </el-input>
+            </el-row>
+            <el-row class='password-info'>
+                <el-col :span='12'>
+                    <el-checkbox v-model="remember">记住密码</el-checkbox>
+                </el-col>
+                <el-col :span='12'>
+                    <el-link type="primary">忘记密码?</el-link>
+                </el-col>
+            </el-row>
+            <el-row class='login-button'>
+                <el-button type="primary">登陆</el-button>
+            </el-row>
+            <el-row class='register'>
+                <el-link type="primary">立即注册</el-link>
+            </el-row>
         </div>
     </div>
 </template>
 
 <script>
-
 
     export default {
         name: 'WelcomeTo',
@@ -31,7 +47,8 @@
                 user: {
                     name: '',
                     password: ''
-                }
+                },
+                remember: false
             };
         }
     };
