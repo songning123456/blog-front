97beea93ed8712d3d3f36705c6e762a486c8904e
songning <songning@kedacom.com>
2019-09-10 19:08:50 +0800

    simple-music 完成

--- src/views/music/SimpleMusic.vue
+++ src/views/music/SimpleMusic.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class='simple-music' v-if='listenMusic'>
+    <div class='simple-music' v-if='listenMusic' :class='getSimpleMusicCss()'>
         <div class='music-header' :class='fullscreen ? "header-full" : "header-no-full"'>
             <span title='小屏' @click='narrow'><img src='../../assets/smallScreen.svg'/></span>
-            <span title='全屏' @click="screenfullClick"><img src='../../assets/fullScreen.svg'/></span>
+            <span v-if='!smallscreen' title='全屏' @click="screenfullClick"><img
+                src='../../assets/fullScreen.svg'/></span>
             <span title='退出' @click='exit'><img src='../../assets/musicExit.svg'/></span>
         </div>
         <div class='music-body' :class='fullscreen ? "body-full" : "body-no-full"'>
@@ -24,24 +25,36 @@
             },
             musicSrc: {
                 type: String,
-                default: 'https://www.cnblogs.com/dudu123/p/10148578.html'
+                default: 'https://y.qq.com/'
             }
         },
         data () {
             return {
                 fullscreen: false,
-                smallscreen: false
+                smallscreen: null
             };
         },
         methods: {
             // 缩小音乐盒子
             narrow () {
-
+                let scope = this;
+                scope.smallscreen = !scope.smallscreen;
             },
             // 退出音乐
             exit () {
                 let scope = this;
+                scope.smallscreen = null;
                 scope.$store.commit('setListenMusic', false);
+            },
+            getSimpleMusicCss () {
+                let scope = this;
+                if (scope.smallscreen === true) {
+                    return 'music-narrow';
+                } else if (scope.smallscreen === false) {
+                    return 'music-normal';
+                } else {
+                    return 'music-size';
+                }
             },
             // 是否全屏
             screenfullClick () {
@@ -73,12 +86,23 @@
 
 <style lang="scss">
     .simple-music {
-        width: 100%;
-        height: 100%;
         z-index: 1000;
         position: absolute;
-        top: 0;
-        left: 0;
+        right: 0;
+        bottom: 0;
+
+        &.music-size {
+            width: 100%;
+            height: 100%;
+        }
+
+        &.music-normal {
+            animation: go_out .4s forwards;
+        }
+
+        &.music-narrow {
+            animation: go_in .4s forwards;
+        }
 
         .music-header {
             width: 100%;
@@ -88,11 +112,11 @@
             align-items: center;
 
             span {
-                padding: 0 18px;
+                padding: 0 .9rem;
                 cursor: pointer;
 
                 img {
-                    transform: scale(1.4);
+                    transform: scale(1.1);
                 }
             }
         }
@@ -103,7 +127,7 @@
         }
 
         .header-no-full {
-            height: 40px;
+            height: 2rem;
             visibility: visible;
         }
 
@@ -112,7 +136,7 @@
         }
 
         .body-no-full {
-            height: calc(100% - 40px);
+            height: calc(100% - 2rem);
         }
 
         .music-body {
@@ -120,9 +144,29 @@
 
             .music-iframe {
                 background-color: rgba(0, 0, 0, 0.8);
-                /*padding: 0 .2rem 0 .2rem;*/
-                /*box-sizing: border-box;*/
             }
+        }
+    }
+
+    @keyframes go_in {
+        0% {
+            width: 100%;
+            height: 100%;
+        }
+        100% {
+            width: 12rem;
+            height: 8rem;
+        }
+    }
+
+    @keyframes go_out {
+        0% {
+            width: 12rem;
+            height: 8rem;
+        }
+        100% {
+            width: 100%;
+            height: 100%;
         }
     }
 </style>
