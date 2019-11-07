5d9025a2e734c6b9d622b6c0ebdac0f13fca6776
songning <songning@kedacom.com>
2019-11-07 11:11:03 +0800

    部署时localhost 改成 服务器ip 修改

--- src/configure/ajax/config.js
+++ src/configure/ajax/config.js
@@ -1,15 +1,15 @@
 
 // 发布模式配置
 let production = {
-    REQUEST_BASE_URL: 'http://122.51.193.191:8072/blog-server',
+    REQUEST_BASE_URL: 'http://122.51.193.191:8072',
     AJAX_TIMEOUT: 30000
 };
 
 // 开发模式配置
-let dev = {
+/*let dev = {
     REQUEST_BASE_URL: 'http://localhost:8072',
     AJAX_TIMEOUT: 100000
-};
+};*/
 
 let Config = production;
 export default Config;
