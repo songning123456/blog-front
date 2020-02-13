6863489817a25288cc84f3accd9c9e93b4f21dbd
songning <1457065857@qq.com>
2020-02-13 14:37:15 +0800

    分片上传优化

--- src/utils/UploadUtil.js
+++ src/utils/UploadUtil.js
@@ -5,6 +5,7 @@ let upload = {};
 export const uploadByPieces = ({file, pieceSize = 10, progress, success, error}) => {
     upload.chunkSize = pieceSize * 1024 * 1024; // 10MB/片
     upload.chunkCount = Math.ceil(file.size / upload.chunkSize); // 总片数
+    upload.hasExist = []; // 某个文件已经上传的部分
     const readFile = () => {
         let fileReader = new FileReader();
         fileReader.readAsBinaryString(file);
@@ -13,8 +14,14 @@ export const uploadByPieces = ({file, pieceSize = 10, progress, success, error})
             upload.md5 = md5(fileBlob);
             videoIsExist({md5: upload.md5}).then(data => {
                 if (data.status === 200) {
-                    // 文件已经上传
-                    success && success(data);
+                    if (!data.data) {
+                        // 文件已经上传
+                        success && success(data);
+                    } else if (data.data && data.data.length > 0) {
+                        // 已经上传的部分
+                        upload.hasExist = data.data;
+                        readChunk();
+                    }
                 } else {
                     readChunk();
                 }
@@ -23,6 +30,7 @@ export const uploadByPieces = ({file, pieceSize = 10, progress, success, error})
             });
         });
     };
+    // 获取分片相关信息
     const getChunkInfo = (file, currentChunk, chunkSize) => {
         let start = currentChunk * chunkSize;
         let end = Math.min(file.size, start + chunkSize);
@@ -32,31 +40,56 @@ export const uploadByPieces = ({file, pieceSize = 10, progress, success, error})
         };
     };
     const readChunk = () => {
+        let promiseAll = [];
         for (let i = 0; i < upload.chunkCount; i++) {
-            const {chunk} = getChunkInfo(file, i, upload.chunkSize);
-            uploadChunk({chunk, currentChunk: i, chunkCount: upload.chunkCount});
+            // 已经存在的分片无需上传
+            if (!upload.hasExist.includes(i)) {
+                const {chunk} = getChunkInfo(file, i, upload.chunkSize);
+                promiseAll.push(uploadChunk({chunk, currentChunk: i}));
+            }
+        }
+        // 存在某些分片未上传
+        if (promiseAll.length > 0) {
+            Promise.all(promiseAll).then(result => {
+                if (!result.includes('fail')) {
+                    // 所有分片都已经上传成功，准备合并
+                    mergeChunk();
+                }
+            }).catch(e => {
+                error && error(e);
+            });
+        } else {
+            // 说明所有的分片已经上传，仅仅未合并
+            mergeChunk();
         }
     };
-    const uploadChunk = ({chunk, currentChunk, chunkCount}) => {
+    // 合并分片
+    const mergeChunk = () => {
+        shardMerge({md5: upload.md5, filename: file.name}).then(data => {
+            if (data.status === 200) {
+                success && success(data);
+            }
+        }).catch(e => {
+            error && error(e);
+        });
+    };
+    // 上传分片
+    const uploadChunk = ({chunk, currentChunk}) => {
         let formData = new FormData();
         formData.append('file', chunk);
         formData.append('md5', upload.md5);
         formData.append('filename', file.name);
         formData.append('currentChunk', currentChunk);
-        shardUpload(formData).then(data => {
-            if (data.status === 200) {
-                if (currentChunk >= chunkCount - 1) {
-                    shardMerge({md5: upload.md5, filename: file.name}).then(data => {
-                        if (data.status === 200) {
-                            success && success(data);
-                        }
-                    }).catch(e => {
-                        error && error(e);
-                    });
+        return new Promise((resolve, reject) => {
+            shardUpload(formData).then(data => {
+                if (data.status === 200) {
+                    resolve('success');
+                } else {
+                    reject('fail');
                 }
-            }
-        }).catch(e => {
-            error && error(e);
+            }).catch(e => {
+                reject('fail');
+            });
         });
     };
     readFile();
--- src/views/modifyInfo/components/ModifyVideo.vue
+++ src/views/modifyInfo/components/ModifyVideo.vue
@@ -51,7 +51,6 @@
                     aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
                     fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
                     sources: [],
-                    techOrder: ['flash'],
                     //你的封面地址
                     poster: require('../../../assets/aiqiyi.svg'),
                     // width: document.documentElement.clientWidth,
